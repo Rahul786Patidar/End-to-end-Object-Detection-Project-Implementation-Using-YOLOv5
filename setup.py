0846074a7b91d6ef03525c36b8638033af136e0e
Rahul786Patidar <rhlptr786@gmail.com>
2024-09-09 08:51:13 +0530

    requirements addedd

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Rahul Patidar',
+    author_email= 'Rhlptr786@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
